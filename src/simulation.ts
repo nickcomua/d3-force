c0c4b0621365476465503646407fae91264d227a
nickp <b.b@b.b>
2025-06-01 20:32:59 +0200

    type fix

--- src/simulation.ts
+++ src/simulation.ts
@@ -19,7 +19,7 @@ const initialRadius = 10,
  */
 export class Simulation<
   NodeDatum extends SimulationNodeDatum,
-  LinkDatum extends SimulationLinkDatum<NodeDatum> | undefined,
+  LinkDatum extends SimulationLinkDatum<NodeDatum>,
 > {
   /**
    * Return the current alpha of the simulation, which defaults to 1.
@@ -345,32 +345,16 @@ export function y(d: SimulationNodeDatum) {
  * The simulator starts automatically; use simulation.on to listen for tick events as the simulation runs.
  * If you wish to run the simulation manually instead, call simulation.stop, and then call simulation.tick as desired.
  *
- * Use this signature, when creating a simulation WITHOUT link force(s).
- *
- * The generic refers to the type of the data for a node.
- *
- * @param nodesData Optional array of nodes data, defaults to empty array.
- */
-export function forceSimulation<NodeDatum extends SimulationNodeDatum>(
-  nodesData?: NodeDatum[],
-): Simulation<NodeDatum, undefined>;
-/**
- * Create a new simulation with the specified array of nodes and no forces.
- * If nodes is not specified, it defaults to the empty array.
- * The simulator starts automatically; use simulation.on to listen for tick events as the simulation runs.
- * If you wish to run the simulation manually instead, call simulation.stop, and then call simulation.tick as desired.
- *
  * Use this signature, when creating a simulation WITH link force(s).
  *
  * The first generic refers to the type of data for a node.
  * The second generic refers to the type of data for a link.
  *
  * @param nodesData Optional array of nodes data, defaults to empty array.
- */
+ */ 
 export function forceSimulation<
-  NodeDatum extends SimulationNodeDatum,
-  // eslint-disable-next-line @definitelytyped/no-unnecessary-generics
-  LinkDatum extends SimulationLinkDatum<NodeDatum>,
+  NodeDatum extends SimulationNodeDatum = SimulationNodeDatum,
+  LinkDatum extends SimulationLinkDatum<NodeDatum> = SimulationLinkDatum<NodeDatum>,
 >(nodesData?: NodeDatum[]): Simulation<NodeDatum, LinkDatum> {
   if (!nodesData) nodesData = [];
   return new Simulation<NodeDatum, LinkDatum>().nodes(nodesData);
